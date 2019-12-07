55ccc9c212073a95cf799bbd0e31a2ac877a67ac
mlovewanshi <mohanlovewanshi@Mohans-MacBook-Pro.local>
2019-12-07 15:30:58 +0530

    adde new instance.

--- workshop/ec2-instance.tf
+++ workshop/ec2-instance.tf
@@ -1,12 +1,12 @@
 resource "aws_key_pair" "ssh_key" {
-  key_name = ""
+  key_name = "instancekey"
   public_key = ""
 }
 
 resource "aws_instance" "first_instance" {
-  ami = ""
+  ami = "ami-04b9e92b5572fa0d1"
 
-  subnet_id = ""
+  subnet_id = "subnet-4a8b8216"
 
   key_name = aws_key_pair.ssh_key.key_name
 
